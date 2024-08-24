4bf37fe949df8e0d210ec7d69d89e153af73926b
Aamir@786 <aamirhussainazad786@gmail.com>
2024-08-24 13:11:23 +0530

    commit add

---
+++ L2VariableAndDataTypes/L2VariableAndDataTypes/L2VariableAndDataTypes/Literals.java
@@ -0,0 +1,36 @@
+public class Literals {
+    public static void main(String[] args) {
+        // Integer Literals
+        int a = 10;
+        int b = 0b1010;
+        int c = 012;
+        int d = 0xA;
+
+        // Float Literals
+        float e = 1.23f;
+        float f = 1.23e2f;
+        float g = 210E-4f;
+
+        // Boolean Literals
+        boolean h = true;
+        boolean i = false;
+
+        // Character Literals
+        char j = 'A';
+        char k = '\u0041';
+
+        // String Literals
+        String l = "Hello";
+        String m = "World";
+
+        System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + h + " " + i + " " + j + " " + k + " " + l + " " + m);
+    }
+}
+
+
+//literal => 
+    // 2*(a + b);
+    // 2 * (a + b) + 3 * (c - d);
+
+    // 2 and 3 is a literal they can not be change
+    //a b c and is a variable 
---
+++ L2VariableAndDataTypes/L2VariableAndDataTypes/L2VariableAndDataTypes/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+ // System.out.println("Enter your long value: ");
+        // long l = input.nextLong();
+        // System.out.println("Your long value is: " + l);
