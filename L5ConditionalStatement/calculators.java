7d1531401f8d0b918e101583ff588097a4f2c4b2
Aamir@786 <aamirhussainazad786@gmail.com>
2024-09-03 15:58:03 +0530

    commit add

--- L5ConditionalStatement/calculators.java
+++ L5ConditionalStatement/calculators.java
@@ -1,5 +1,61 @@
 
 
+// import java.util.Scanner;
+
+// public class calculators {
+
+//     public static void main(String[] args) {
+//         @SuppressWarnings("resource")
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter first number: ");
+//         double a = sc.nextDouble();
+//         System.out.print("Enter second number: ");
+//         double b = sc.nextDouble();
+
+//         System.out.print("Enter operator: ");
+//         char operator = sc.next().charAt(0);
+
+//         switch (operator) {
+//             case '+':
+//                 System.out.printf("%.2f + %.2f = %.2f%n", a, b, a + b);
+//                 break;
+//             case '-':
+//                 System.out.printf("%.2f - %.2f = %.2f%n", a, b, a - b);
+//                 break;
+//             case '*':
+//                 System.out.printf("%.2f * %.2f = %.2f%n", a, b, a * b);
+//                 break;
+//             case '/':
+//                 if (b == 0) {
+//                     System.out.println("Error: Division by zero is not allowed.");
+//                 } else {
+//                     System.out.printf("%.2f / %.2f = %.2f%n", a, b, a / b);
+//                 }
+//                 break;
+//             case '%':
+//                 if (b == 0) {
+//                     System.out.println("Error: Modulo by zero is not allowed.");
+//                 } else {
+//                     System.out.printf("%.2f mod %.2f = %.2f%n", a, b, a % b);
+//                 }
+//                 break; // Added break here
+//             default:
+//                 System.out.println("Error: Invalid operator. Please use +, -, *, / or %.");
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 import java.util.Scanner;
 
 public class calculators {
@@ -16,31 +72,25 @@ public class calculators {
         char operator = sc.next().charAt(0);
 
         switch (operator) {
-            case '+':
-                System.out.printf("%.2f + %.2f = %.2f%n", a, b, a + b);
-                break;
-            case '-':
-                System.out.printf("%.2f - %.2f = %.2f%n", a, b, a - b);
-                break;
-            case '*':
-                System.out.printf("%.2f * %.2f = %.2f%n", a, b, a * b);
-                break;
-            case '/':
+            case '+' -> System.out.printf("%.2f + %.2f = %.2f%n", a, b, a + b);
+            case '-' -> System.out.printf("%.2f - %.2f = %.2f%n", a, b, a - b);
+            case '*' -> System.out.printf("%.2f * %.2f = %.2f%n", a, b, a * b);
+            case '/' -> {
                 if (b == 0) {
                     System.out.println("Error: Division by zero is not allowed.");
                 } else {
                     System.out.printf("%.2f / %.2f = %.2f%n", a, b, a / b);
                 }
-                break;
-            case '%':
+            }
+            case '%' -> {
                 if (b == 0) {
                     System.out.println("Error: Modulo by zero is not allowed.");
                 } else {
                     System.out.printf("%.2f mod %.2f = %.2f%n", a, b, a % b);
                 }
-                break; // Added break here
-            default:
-                System.out.println("Error: Invalid operator. Please use +, -, *, / or %.");
+                // Added break here
+            }
+            default -> System.out.println("Error: Invalid operator. Please use +, -, *, / or %.");
         }
     }
 }
