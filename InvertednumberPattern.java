eba8bc45fabd0ec5d972a3f0d85cf1149af1bfab
Aamir@786 <aamirhussainazad786@gmail.com>
2024-09-08 16:44:49 +0530

    commit add

---
+++ InvertednumberPattern.java
@@ -0,0 +1,22 @@
+// package Advance-DSA;
+
+import java.util.Scanner;
+
+public class InvertednumberPattern {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.err.println("Enter a number ");
+
+            int n = sc.nextInt();
+            for (int line = 1; line <= n; line++) {
+                for (int number = line; number >=1; number--) {
+                    System.out.print(number + " ");
+                }
+                System.err.println();
+            }
+        } catch (Exception error) {
+            System.err.println("Error: " + error.getMessage());
+        }
+    }
+}
---
+++ numberPattern.java
@@ -0,0 +1,49 @@
+// package Advance-DSA;
+
+// import java.util.Scanner;
+
+// public class numberPattern {
+
+//     public static void main(String[] args) {
+//         try (Scanner sc = new Scanner(System.in)) {
+//             System.err.println("Enter a number ");
+
+//             int n = sc.nextInt();
+//             for (int line = 1; line <= n; line++) {
+//                 for (int number = 1; number <= line; number++) {
+//                     System.out.print(number + " ");
+//                 }
+//                 System.err.println();
+//             }
+//         }catch(Exception error){
+//             System.err.println("Error: " + error.getMessage());
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+import java.util.Scanner;
+
+public class numberPattern {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.err.println("Enter a number ");
+
+            int n = sc.nextInt();
+            for (int line = 1; line <= n; line++) {
+                for (int number = 1; number <= line; number++) {
+                    System.out.print(number + " ");
+                }
+                System.err.println();
+            }
+        }catch(Exception error){
+            System.err.println("Error: " + error.getMessage());
+        }
+    }
+}
