326e6d21ecd71729624ec72dce3df4fb38ee8d7a
Aamir@786 <aamirhussainazad786@gmail.com>
2024-09-08 21:18:38 +0530

    commit

--- L2week2PracticeSession1/practice4LiveSunday/Butterfly.java
+++ L2week2PracticeSession1/practice4LiveSunday/Butterfly.java
@@ -52,56 +52,55 @@ import java.util.Scanner;
 
 public class Butterfly {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the number of rows for the butterfly pattern: ");
-        int n = sc.nextInt();
-        int st = 1;
-        int sp = 2 * n - 2;
-
-        // Upper half of the butterfly
-        for (int number_of_line = 1; number_of_line <= n; number_of_line++) {
-            // Print stars on the left side
-            for (int i = 1; i <= st; i++) {
-                System.out.print("*");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the number of rows for the butterfly pattern: ");
+            int n = sc.nextInt();
+            int st = 1;
+            int sp = 2 * n - 2;
+            
+            // Upper half of the butterfly
+            for (int number_of_line = 1; number_of_line <= n; number_of_line++) {
+                // Print stars on the left side
+                for (int i = 1; i <= st; i++) {
+                    System.out.print("*");
+                }
+                // Print spaces in the middle
+                for (int i = 1; i <= sp; i++) {
+                    System.out.print(" ");
+                }
+                // Print stars on the right side
+                for (int i = 1; i <= st; i++) {
+                    System.out.print("*");
+                }
+                
+                // Increase the number of stars and decrease spaces for the next line
+                st++;
+                sp -= 2;
+                System.out.println();
             }
-            // Print spaces in the middle
-            for (int i = 1; i <= sp; i++) {
-                System.out.print(" ");
+            
+            // Lower half of the butterfly
+            st = n;
+            sp = 0;
+            for (int number_of_line = 1; number_of_line <= n; number_of_line++) {
+                // Print stars on the left side
+                for (int i = 1; i <= st; i++) {
+                    System.out.print("*");
+                }
+                // Print spaces in the middle
+                for (int i = 1; i <= sp; i++) {
+                    System.out.print(" ");
+                }
+                // Print stars on the right side
+                for (int i = 1; i <= st; i++) {
+                    System.out.print("*");
+                }
+                
+                // Decrease the number of stars and increase spaces for the next line
+                st--;
+                sp += 2;
+                System.out.println();
             }
-            // Print stars on the right side
-            for (int i = 1; i <= st; i++) {
-                System.out.print("*");
-            }
-
-            // Increase the number of stars and decrease spaces for the next line
-            st++;
-            sp -= 2;
-            System.out.println();
         }
-
-        // Lower half of the butterfly
-        st = n;
-        sp = 0;
-        for (int number_of_line = 1; number_of_line <= n; number_of_line++) {
-            // Print stars on the left side
-            for (int i = 1; i <= st; i++) {
-                System.out.print("*");
-            }
-            // Print spaces in the middle
-            for (int i = 1; i <= sp; i++) {
-                System.out.print(" ");
-            }
-            // Print stars on the right side
-            for (int i = 1; i <= st; i++) {
-                System.out.print("*");
-            }
-
-            // Decrease the number of stars and increase spaces for the next line
-            st--;
-            sp += 2;
-            System.out.println();
-        }
-        sc.close();
     }
 }
