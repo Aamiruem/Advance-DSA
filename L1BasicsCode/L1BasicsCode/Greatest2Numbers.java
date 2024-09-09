f1c80f99154288413c217874c765dacc6d66255c
Aamir@786 <aamirhussainazad786@gmail.com>
2024-09-09 16:25:47 +0530

    COMMIT ADD

--- L1BasicsCode/L1BasicsCode/Greatest2Numbers.java
+++ L1BasicsCode/L1BasicsCode/Greatest2Numbers.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 public class Greatest2Numbers {
     public static void main(String[] args) {
 
-        // int a = 30;
-        // int b = 20;
-        Scanner sc = new Scanner(System.in);
+        try (// int a = 30;
+                // int b = 20;
+        Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter first number: ");
+            int a = sc.nextInt();
 
-        System.out.println("Enter first number: ");
-        int a = sc.nextInt();
+            System.out.println("Enter second number: ");
+            int b = sc.nextInt();
 
-        System.out.println("Enter second number: ");
-        int b = sc.nextInt();
-
-        if (a > b) {
-            System.out.println(a + " is greater than " + b);
-        } else {
-            System.out.println(b + " is greater than " + a);
+            if (a > b) {
+                System.out.println(a + " is greater than " + b);
+            } else {
+                System.out.println(b + " is greater than " + a);
+            }
         }
     }
 }
---
+++ L7Pattern/EvenSumAndOddSum.java
@@ -0,0 +1,84 @@
+// // write  two numbers sum
+
+// import java.util.Scanner;
+
+// public class EvenSumAndOddSum {
+
+//     public static void main(String[] args) {
+
+//         Scanner input;
+//         input = new Scanner(System.in);
+//         int choice;
+//         int evenSum = 0;
+//         int oddSum = 0;
+//         int evenNum = 0;
+//         int oddNum = 0;
+
+//         do {
+//             System.out.println("Enter the number: ");
+
+//             int number = input.nextInt();
+//             if (number % 2 == 0) {
+//                 evenSum += number;
+//             } else {
+//                 oddSum += number;
+//             }
+//             System.out.println("Do you want to continue? ( pres 1 for Yes, and 0 for No)?");
+//             System.out.println("Enter your choice: ");
+//             choice = input.nextInt();
+
+//         } while (choice == 1);
+
+//         System.out.println("Sum of even numbers upto: " + evenNum + evenSum);
+//         System.out.println("Sum of odd numbers: upto "+ oddNum + oddSum);
+//         input.close();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+import java.util.Scanner;
+
+public class EvenSumAndOddSum {
+
+    public static void main(String[] args) {
+
+        try (Scanner input = new Scanner(System.in)) {
+            int choice;
+            int evenSum = 0;
+            int oddSum = 0;
+            int evenCount = 0;
+            int oddCount = 0;
+            
+            do {
+                System.out.println("Enter the number: ");
+                int number = input.nextInt();
+                
+                if (number % 2 == 0) {
+                    evenSum += number;
+                    evenCount++;
+                } else {
+                    oddSum += number;
+                    oddCount++;
+                }
+                
+                System.out.println("Do you want to continue? (press 1 for Yes, and 0 for No): ");
+                choice = input.nextInt();
+                
+            } while (choice == 1);
+            
+            // Print the sums of even and odd numbers
+            System.out.println("Sum of even numbers: " + evenSum);
+            System.out.println("Total number of even numbers entered: " + evenCount);
+            System.out.println("Sum of odd numbers: " + oddSum);
+            System.out.println("Total number of odd numbers entered: " + oddCount);
+        }
+    }
+}
