2b1fcb439e9d81642131f2e616bbb56635839baf
Aamir Hussain <aamirhussainazad786@gmail.com>
2024-09-16 12:00:31 +0530

    commit add

--- L9FunctionPractice/L9FunctionPractice/Average3Sums.java
+++ L9FunctionPractice/L9FunctionPractice/Average3Sums.java
@@ -1,5 +1,23 @@
-package L9FunctionPractice;
+// package L9FunctionPractice;
+
+import java.util.Scanner;
 
 public class Average3Sums {
-    
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter the first number: ");
+            int x = input.nextInt();
+            System.out.print("Enter the second number: ");
+            int y = input.nextInt();
+            System.out.print("Enter the third number: ");
+            int z = input.nextInt();
+            double average = (x + y + z) / 3;
+            System.out.println("Average is: " + average);
+        }
+    }
+
+    public static double findAverage(int a, int b, int c) {
+        double average = (a + b + c) / 3;
+        return average;
+    }
 }
